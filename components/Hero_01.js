bbb6791fd3327a8498a2e0ad294fa79efc78eeef
Rownok <rownok2001@gmail.com>
2023-03-06 18:54:49 +0600

    update

--- components/Hero_01.js
+++ components/Hero_01.js
@@ -21,10 +21,10 @@ const Hero_01 = () => {
       <div className="container gap-5 xl:justify-between px-20 py-16 rounded-3xl items-center flex flex-col xl:flex-row  ">
       <div className="flex flex-col gap-10 md:flex-row justify-center md:justify-between items-center">
         <div className="flex flex-col gap-10  items-center md:items-start">
-          <p className="text-3xl md:text-5xl lg:text-6xl font-semibold dark:text-white text-center md:text-start">Celebrate <span className="text-[#056691]">Ramadan</span>  <br /> With Everyone</p>
+          <p className="text-3xl md:text-5xl lg:text-6xl font-semibold dark:text-white text-center md:text-start">Celebrate <span className="text-[#556bac]">Ramadan</span>  <br /> With Everyone</p>
           <p className="text-base md:text-lg  text-center md:text-start">Ramadan is a holy month for Muslims. It is when they fast, pray, <br /> reflect and do good deeds1. Ramadan follows the lunar calendar, <br /> so it changes every year2. In 2023, it will start on March 23.</p>
           <div className="flex flex-col md:flex-row gap-5 items-center">
-            <button onClick={openModal}  className="bg-[#056691] flex items-center gap-2 px-4 py-3 font-medium hover:scale-95 duration-300 transition shadow-xl shadow-[#056691]/30 hover:shadow-[#ecedc7] text-white hover:bg-[#ecc79e] rounded-full hover:text-black capitalize text-lg">Donate People
+            <button onClick={openModal}  className="bg-[#556bac] flex items-center gap-2 px-4 py-3 font-medium hover:scale-95 duration-300 transition shadow-xl shadow-[#056691]/30 hover:shadow-[#ecedc7] text-white hover:bg-[#ecc79e] rounded-full hover:text-black capitalize text-lg">Donate People
             <BiDonateHeart/>
             </button>
             <button className="text-xl capitalize font-semibold">How It Works</button>
