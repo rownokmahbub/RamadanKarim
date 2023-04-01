f503640bf0178804641dd9123802c659bfd95cab
rownok <rownok2001@gmail.com>
2023-04-01 22:49:02 +0600

    dfjhdf

--- components/Donate.js
+++ components/Donate.js
@@ -4,7 +4,7 @@ const Donate = () => {
     const notify = () =>toast.success('Successfully created!');;
   return (
     <div>
-    <button onClick={notify}>Make me a toast</button>
+    <button onClick={notify}>Make me a df</button>
     <Toaster />
   </div>
   )
