d056264e7c9479ffed45f052842b2c9bf3428d2e
rownok <rownok2001@gmail.com>
2023-04-10 11:47:22 +0600

    make responsive

--- components/Hero_01.js
+++ components/Hero_01.js
@@ -17,13 +17,13 @@ const Hero_01 = () => {
 
   return (
     <div className=" mx-auto bg-gradient-to-r from-indigo-50 via-purple-50 to-pink-50 py-10">
-      <div className="container gap-5 xl:justify-between px-20 py-16 rounded-3xl items-center flex flex-col xl:flex-row  ">
+      <div className="container gap-5 xl:justify-between px-2 md:px-20 py-16 rounded-3xl items-center flex flex-col xl:flex-row  ">
       <div className="flex flex-col gap-10 md:flex-row justify-center md:justify-between items-center">
         <div className="flex flex-col gap-10  items-center md:items-start">
           <p className="text-3xl md:text-5xl lg:text-6xl font-semibold  text-center md:text-start">Celebrate <span className="text-primary dark:text-primary-50">Ramadan</span>  <br /> With Everyone</p>
           <p className="text-base md:text-lg  text-center md:text-start">Ramadan is a holy month for Muslims. It is when they fast, pray, <br /> reflect and do good deeds1. Ramadan follows the lunar calendar, <br /> so it changes every year2. In 2023, it will start on March 23.</p>
           <div className="flex flex-col md:flex-row gap-5 items-center">
-            <button onClick={openModal}  className="bg-primary dark:bg-primary-50 flex items-center gap-2 px-4 py-3 font-medium hover:scale-95 duration-300 transition shadow-xl shadow-primary/30 hover:shadow-[#ecedc7] dark:shadow-primary-50/20 text-white hover:bg-[#ecc79e] rounded-full hover:text-black capitalize text-lg">Donate People
+            <button onClick={openModal}  className="bg-primary dark:bg-primary-50 flex items-center gap-2 px-3 md:px-4 py-2 md:py-3 font-medium hover:scale-95 duration-300 transition shadow-xl shadow-primary/30 hover:shadow-[#ecedc7] dark:shadow-primary-50/20 text-white hover:bg-[#ecc79e] rounded-full hover:text-black capitalize text-lg">Donate People
             <BiDonateHeart/>
             </button>
 
