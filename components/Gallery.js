cc81bbc8d6262b484f1f376602a0557fc662e202
rownok <rownok2001@gmail.com>
2023-04-10 21:18:38 +0600

    make gallery

--- components/Gallery.js
+++ components/Gallery.js
@@ -6,18 +6,18 @@ const Gallery = () => {
         <div className="container mx-auto">
         <p className='text-2xl md:text-4xl font-semibold flex justify-center text-center my-20'>Let's make these people happy <br /> with food in ramadan</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center">
-            <img className='rounded-2xl' src="/assets/g1.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g2.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g3.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g4.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g5.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g6.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g7.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g1.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g7.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g2.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g5.jpg" alt="g1" />
-            <img className='rounded-2xl' src="/assets/g6.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g1.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g2.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g3.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g4.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g5.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g6.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g7.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g1.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g7.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g2.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g5.jpg" alt="g1" />
+            <img className='rounded-2xl dark:hue-rotate-180' src="/assets/g6.jpg" alt="g1" />
         </div>
         </div>
       
